9881cb60da1251a59cb122595f492065624f01f2
dopa <edu.riveros@duocuc.cl>
2026-08-19 14:10:54 -0400

    Actividad Semana 2 - SpeedFast

--- src/app/Main.java
+++ src/app/Main.java
@@ -8,11 +8,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        PedidoComida comida = new PedidoComida("001", "San Bernardo", 2);
+        PedidoComida comida = new PedidoComida("0001", "San Bernardo", 2);
 
-        PedidoEncomienda encomienda = new PedidoEncomienda("002", "Buin", 6);
+        PedidoEncomienda encomienda = new PedidoEncomienda("0002", "Buin", 6);
 
-        PedidoExpress express = new PedidoExpress("003", "Av. Italia 123", 12);
+        PedidoExpress express = new PedidoExpress("0003", "Av. Italia 123", 12);
 
         comida.mostrarResumen();
         comida.calcularTiempoEntrega();
